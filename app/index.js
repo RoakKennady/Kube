f3dafc405fc7d00230479995422c6a092a79b8e9
RoakKennady <93422490+RoakKennady@users.noreply.github.com>
2022-12-20 17:20:54 +0530

    first commit

---
+++ app/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => res.send('Hello World'))
+
+app.listen(80, '0.0.0.0',() => console.log('Example app Listening on port 3000!'))
+
